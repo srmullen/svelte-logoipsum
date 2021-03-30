13f3fcfc9651a0a6b7e595225f6c02396d6093a9
Sean Mullen <srmullen13@gmail.com>
2021-03-30 17:14:44 -0400

    minimal docs

--- README.md
+++ README.md
@@ -19,9 +19,16 @@ or yarn
 </script>
 
 <Logo1 />
-<Logo16 color1="#2a9d8f" />
+<Logo1 color="red" />
+<Logo16 color1="#2a9d8f" color2="#ffd200" />
 ```
+
+All components expose properties to change each color. Most logos are just one color so they just have a `color` property. If a logo has more than one color then the colors are numbered (i.e. `color1="#2a9d8f" color2="#ffd200"`). There's no real rhyme or reason to which color is assigned to which number, so just play around until you find what you want. :)
 
 ## Acknowledgements
 
 All logo designs are from [logoipsum by SignalSupply](https://logoipsum.com/).
+
+### TODO
+
+- Github page for showing component usage.
--- docs/src/App.svelte
+++ docs/src/App.svelte
@@ -1,153 +1,53 @@
 <script>
-	import { 
-		Logo1, 
-		Logo2,
-		Logo3,
-		Logo4,
-		Logo5,
-		Logo6,
-		Logo7,
-		Logo8,
-		Logo9,
-		Logo10,
-		Logo11,
-		Logo12,
-		Logo13,
-		Logo14,
-		Logo15,
-		Logo16,
-		Logo17,
-		Logo18,
-		Logo19,
-		Logo20,
-		Logo21,
-		Logo22,
-		Logo23,
-		Logo24,
-		Logo25,
-		Logo26
-	} from 'svelte-logoipsum';
+	import * as Logos from 'svelte-logoipsum';
+
+	const logos = [];
+	for (let i = 1; i <= 26; i++) {
+		const name = `Logo${i}`;
+		logos.push({ name: `<${name} />` , component: Logos[name] });
+	}
 </script>
 
 <main>
-	<div class="logo">
-		<span>Logo1</span>
-		<div>
-			<Logo1 />
-		</div>
-	</div>
-	<div class="logo">
-		<span>Logo2</span>
-		<Logo2 />
-	</div>
-	<div class="logo">
-		<span>Logo3</span>
-		<Logo3 />
-	</div>
-	<div class="logo">
-		<span>Logo4</span>
-		<Logo4 />
-	</div>
-	<div class="logo">
-		<span>Logo5</span>
-		<Logo5 />
-	</div>
-	<div class="logo">
-		<span>Logo6</span>
-		<Logo6 />
-	</div>
-	<div class="logo">
-		<span>Logo7</span>
-		<Logo7 />
-	</div>
-	<div class="logo">
-		<span>Logo8</span>
-		<Logo8 />
-	</div>
-	<div class="logo">
-		<span>Logo9</span>
-		<Logo9 />
-	</div>
-	<div class="logo">
-		<span>Logo10</span>
-		<Logo10 color1="grey" color2="white" />
-	</div>
-	<div class="logo">
-		<span>Logo11</span>
-		<Logo11 />
-	</div>
-	<div class="logo">
-		<span>Logo12</span>
-		<Logo12 color1="red" color2="blue" color3="green" />
-	</div>
-	<div class="logo">
-		<span>Logo13</span>
-		<Logo13 />
-	</div>
-	<div class="logo">
-		<span>Logo14</span>
-		<Logo14 />
-	</div>
-	<div class="logo">
-		<span>Logo15</span>
-		<Logo15 />
-	</div>
-	<div class="logo">
-		<span>Logo16</span>
-		<Logo16 color1="#e9c46a" color2="#f4a261" color3="#2a9d8f" color4="#e76f51" />
-	</div>
-	<div class="logo">
-		<span>Logo17</span>
-		<Logo17 />
-	</div>
-	<div class="logo">
-		<span>Logo18</span>
-		<Logo18 />
-	</div>
-	<div class="logo">
-		<span>Logo19</span>
-		<Logo19 />
-	</div>
-	<div class="logo">
-		<span>Logo20</span>
-		<Logo20 />	
-	</div>
-	<div class="logo">
-		<span>Logo21</span>
-		<Logo21 />
-	</div>
-	<div class="logo">
-		<span>Logo22</span>
-		<Logo22 />
-	</div>
-	<div class="logo">
-		<span>Logo23</span>
-		<Logo23 />
-	</div>
-	<div class="logo">
-		<span>Logo24</span>
-		<Logo24 />
-	</div>
-	<div class="logo">
-		<span>Logo25</span>
-		<Logo25 />
-	</div>
-	<div class="logo">
-		<span>Logo26</span>
-		<Logo26 />
+	<h1>svelte-logoipsum</h1>
+	<div class="logos">
+		{#each logos as logo}
+			<div class="logo">
+				<span>{logo.name}</span>
+				<div>
+					<svelte:component this={logo.component} />
+				</div>
+			</div>
+		{/each}
 	</div>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
 
+	h1 {
+		text-align: center;
+	}
+
+	.logos {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+	}
+
 	.logo {
+		border: 1px solid black;
 		margin: 1.5rem 0;
+		min-width: 240px;
+		margin: 12px;
+		padding: 24px;
+		border-radius: 6px;
+		
 	}
 
 	.logo span {
@@ -158,6 +58,24 @@
 	@media (min-width: 640px) {
 		main {
 			max-width: 500px;
+		}
+	}
+
+	@media (min-width: 1024px) {
+		main {
+			max-width: 800px;
+		}
+	}
+
+	@media (min-width: 1280px) {
+		main {
+			max-width: 1000px;
+		}
+	}
+
+	@media (min-width: 1280px) {
+		main {
+			max-width: 1000px;
 		}
 	}
 </style>
